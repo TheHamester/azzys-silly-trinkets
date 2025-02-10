3e4735f2f72fa5d4d31c7623ca6fb8654a89322d
Hamester <arigadon2000@gmail.com>
2025-02-11 01:58:39 +0300

    pretty comments

--- src/blind.lua
+++ src/blind.lua
@@ -1,5 +1,11 @@
+------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Azzy's Silly Trinkets (AST)
+------------------------------------------------------------------------------------------------------------------------------------------------------
 -- blind.lua 
+-- Registering of modded boss blinds, as well as relevant hooks for implementing them
+--
+-- Hamester, 2025
+------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Registering Atlas
@@ -17,7 +23,11 @@ SMODS.Atlas {
 -- Registering modded boss blinds
 ------------------------------------------------------------------------------------------------------------------------------------------------------
 
--- Registering The Clock
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- The Clock
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Registering
 SMODS.Blind {
 	key = AST.BLIND.THE_CLOCK.NAME,
 	atlas = AST.BLIND.ATLAS,
@@ -49,180 +59,6 @@ SMODS.Blind {
 	end
 }
 
--- Registering The Razor
-SMODS.Blind {
-	key = AST.BLIND.THE_RAZOR.NAME,
-	atlas = AST.BLIND.ATLAS,
-	pos = { x = 0, y = AST.BLIND.THE_RAZOR.ATLAS_ROW },
-	boss_colour = AST.BLIND.THE_RAZOR.COLOR,
-	dollars = AST.BLIND.THE_RAZOR.REWARD,
-	mult = AST.BLIND.THE_RAZOR.BASE_MULT,
-	boss = { min = AST.BLIND.THE_RAZOR.BOSS_MIN, max = AST.BLIND.THE_RAZOR.BOSS_MAX },
-	card_scored = function(_, card)
-		local suit_prefix = string.sub(card.base.suit, 1, 1)..'_'
-		local rank_suffix = card.base.id == 2 and 14 or math.min(card.base.id - 1, 14)
-
-		if rank_suffix < 10 then rank_suffix = tostring(rank_suffix)
-		elseif rank_suffix == 10 then rank_suffix = 'T'
-		elseif rank_suffix == 11 then rank_suffix = 'J'
-		elseif rank_suffix == 12 then rank_suffix = 'Q'
-		elseif rank_suffix == 13 then rank_suffix = 'K'
-		elseif rank_suffix == 14 then rank_suffix = 'A'
-		end
-		G.E_MANAGER:add_event(Event({trigger = 'after', blocking = false, delay = 0.0, func = function()
-			card:juice_up(0.3, 0.5)
-			return true
-		  end
-		}))
-		card:set_base(G.P_CARDS[suit_prefix..rank_suffix])
-
-		return true
-	end
-}
-
--- Registering The Insecurity
-SMODS.Blind {
-	key = AST.BLIND.THE_INSECURITY.NAME,
-	atlas = AST.BLIND.ATLAS,
-	pos = { x = 0, y = AST.BLIND.THE_INSECURITY.ATLAS_ROW },
-	boss_colour = AST.BLIND.THE_INSECURITY.COLOR,
-	dollars = AST.BLIND.THE_INSECURITY.REWARD,
-	mult = AST.BLIND.THE_INSECURITY.BASE_MULT,
-	boss = { min = AST.BLIND.THE_INSECURITY.BOSS_MIN, max = AST.BLIND.THE_INSECURITY.BOSS_MAX },
-	recalc_debuff = function(_, _, _)
-		if G.GAME.current_round.last_obtained_joker_unique_val ~= 0 then
-			local joker = AST.find_joker_by_unique_val(G.GAME.current_round.last_obtained_joker_unique_val)
-			if joker then
-				joker:set_debuff(true)
-				joker:juice_up()
-			end
-		end
-	end,
-	disabled = function(_)
-		if G.GAME.current_round.last_obtained_joker_unique_val ~= 0 then
-			local joker = AST.find_joker_by_unique_val(G.GAME.current_round.last_obtained_joker_unique_val)
-			if joker then
-				joker:set_debuff(false)
-				joker:juice_up()
-			end
-		end
-	end,
-	defeat = function(_)
-		if G.GAME.current_round.last_obtained_joker_unique_val ~= 0 then
-			local joker = AST.find_joker_by_unique_val(G.GAME.current_round.last_obtained_joker_unique_val)
-			if joker then
-				joker:set_debuff(false)
-				joker:juice_up()
-			end
-		end
-	end
-}
-
--- Registering The Gambit
-SMODS.Blind {
-	key = AST.BLIND.THE_GAMBIT.NAME,
-	atlas = AST.BLIND.ATLAS,
-	pos = { x = 0, y = AST.BLIND.THE_GAMBIT.ATLAS_ROW },
-	boss_colour = AST.BLIND.THE_GAMBIT.COLOR,
-	dollars = AST.BLIND.THE_GAMBIT.REWARD,
-	mult = AST.BLIND.THE_GAMBIT.BASE_MULT,
-	boss = { min = AST.BLIND.THE_GAMBIT.BOSS_MIN, max = AST.BLIND.THE_GAMBIT.BOSS_MAX },
-	card_scored = function(self, card)
-		if card:is_face() then
-			G.E_MANAGER:add_event(Event({trigger = 'after', blocking = false, delay = 0.1, func = function()
-				card:juice_up(0.3, 0.5)
-				ease_dollars(-G.GAME.dollars, true)
-				return true
-				end
-			}))
-			return false
-        end
-		return true
-	end
-}
-
--- Registering The Pit
-SMODS.Blind {
-	key = AST.BLIND.THE_PIT.NAME,
-	atlas = AST.BLIND.ATLAS,
-	pos = { x = 0, y = AST.BLIND.THE_PIT.ATLAS_ROW },
-	boss_colour = AST.BLIND.THE_PIT.COLOR,
-	dollars = AST.BLIND.THE_PIT.REWARD,
-	mult = AST.BLIND.THE_PIT.BASE_MULT,
-	boss = { min = AST.BLIND.THE_PIT.BOSS_MIN, max = AST.BLIND.THE_PIT.BOSS_MAX },
-	card_scored = function(_, card)
-		if card:get_id() == 2 or card:get_id() == 3 or card:get_id() == 4 or card:get_id() == 5 then
-			G.E_MANAGER:add_event(Event({trigger = 'after', blocking = false, delay = 0.1, func = function()
-				card:juice_up(0.3, 0.5)
-				ease_dollars(-G.GAME.dollars, true)
-				return true
-				end
-			}))
-			return false
-        end
-		return true
-	end
-}
-
--- Registering The Construct
-SMODS.Blind {
-	key = AST.BLIND.THE_CONSTRUCT.NAME,
-	atlas = AST.BLIND.ATLAS,
-	pos = { x = 0, y = AST.BLIND.THE_CONSTRUCT.ATLAS_ROW },
-	boss_colour = AST.BLIND.THE_CONSTRUCT.COLOR,
-	dollars = AST.BLIND.THE_CONSTRUCT.REWARD,
-	mult = AST.BLIND.THE_CONSTRUCT.BASE_MULT,
-	boss = { min = AST.BLIND.THE_CONSTRUCT.BOSS_MIN, max = AST.BLIND.THE_CONSTRUCT.BOSS_MAX },
-	recalc_debuff = function(_, card, _)
-		return card.area ~= G.jokers and AST.is_prime(card.base.nominal)
-	end
-}
-
--- Hooking to Game.init_game_object to register extra data for the boss blinds
-local igo = Game.init_game_object
-function Game:init_game_object()
-	local ret = igo()
-	ret.current_round.the_clock = {
-		remaining_time = 0,
-		paused = true,
-		timer_ui_text = nil,
-		timer_text = '0:00',
-		hand_is_being_played = false
-	}
-	ret.current_round.last_obtained_joker_unique_val = 0
-	return ret
-end
-
-------------------------------------------------------------------------------------------------------------------------------------------------------
--- The Insecurity hooks and extra functions
-------------------------------------------------------------------------------------------------------------------------------------------------------
--- Hooking into Card.add_to_deck to get the unique id of most recently obtained joker for The Insecurity boss blind
-local add_to_deck_old = Card.add_to_deck
-function Card:add_to_deck(from_debuff)
-	local ret = add_to_deck_old(self, from_debuff)
-
-	if not from_debuff and self.ability.set == 'Joker' then
-		G.GAME.current_round.last_obtained_joker_unique_val = self.unique_val
-	end
-
-	return ret
-end
-
--- Hooking into Card.remove_from_deck to set last_obtained_joker_unique_val back to 0 when it's removed from the deck
-local remove_from_deck_old = Card.remove_from_deck
-function Card:remove_from_deck(from_debuff)
-	local ret = remove_from_deck_old(self, from_debuff)
-
-	if not from_debuff and G.GAME.current_round.last_obtained_joker_unique_val == self.unique_val and self.ability.set == 'Joker' then
-		G.GAME.current_round.last_obtained_joker_unique_val = 0
-	end
-
-	return ret
-end
-
-------------------------------------------------------------------------------------------------------------------------------------------------------
--- The Clock hooks and extra functions
-------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Callback for updating timer text for DynaText UI object
 G.FUNCS.ui_set_timer_text = function(e)
 	local new_timer_text = "0:0"..math.max((math.floor(G.GAME.current_round.the_clock.remaining_time * 100) / 100), 0)
@@ -287,7 +123,7 @@ local g_update_func = Game.update
 function Game:update(dt)
 	g_update_func(self, dt)
 
-	if G.GAME.blind and G.GAME.blind.name == "bl_ast_the_clock" and not G.GAME.blind.disabled then
+	if G.GAME.blind and G.GAME.blind.name == AST.BLIND.THE_CLOCK.KEY and not G.GAME.blind.disabled then
 		-- Turns out the whole game object is being saved when you quit out of the game (asterisk). some objects, for some reason, during reading
 		-- from the file, are being replaced by the string [["]].."MANUAL_REPLACE"..[["]], this is such a weird way to go around the bug(?)..
 		if not G.GAME.current_round.the_clock.timer_ui_text or G.GAME.current_round.the_clock.timer_ui_text == [["]].."MANUAL_REPLACE"..[["]] then
@@ -311,3 +147,205 @@ function Game:update(dt)
 		end
 	end
 end
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- The Razor
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Registering
+SMODS.Blind {
+	key = AST.BLIND.THE_RAZOR.NAME,
+	atlas = AST.BLIND.ATLAS,
+	pos = { x = 0, y = AST.BLIND.THE_RAZOR.ATLAS_ROW },
+	boss_colour = AST.BLIND.THE_RAZOR.COLOR,
+	dollars = AST.BLIND.THE_RAZOR.REWARD,
+	mult = AST.BLIND.THE_RAZOR.BASE_MULT,
+	boss = { min = AST.BLIND.THE_RAZOR.BOSS_MIN, max = AST.BLIND.THE_RAZOR.BOSS_MAX },
+
+	-- Extra function, added via a lovely patch, which runs for each scored card
+	card_scored = function(_, card)
+		local suit_prefix = string.sub(card.base.suit, 1, 1)..'_'
+		local rank_suffix = card.base.id == 2 and 14 or math.min(card.base.id - 1, 14)
+
+		if rank_suffix < 10 then rank_suffix = tostring(rank_suffix)
+		elseif rank_suffix == 10 then rank_suffix = 'T'
+		elseif rank_suffix == 11 then rank_suffix = 'J'
+		elseif rank_suffix == 12 then rank_suffix = 'Q'
+		elseif rank_suffix == 13 then rank_suffix = 'K'
+		elseif rank_suffix == 14 then rank_suffix = 'A'
+		end
+		G.E_MANAGER:add_event(Event({trigger = 'after', blocking = false, delay = 0.0, func = function()
+			card:juice_up(0.3, 0.5)
+			return true
+		  end
+		}))
+		card:set_base(G.P_CARDS[suit_prefix..rank_suffix])
+
+		return true
+	end
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- The Insecurity
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Registering
+SMODS.Blind {
+	key = AST.BLIND.THE_INSECURITY.NAME,
+	atlas = AST.BLIND.ATLAS,
+	pos = { x = 0, y = AST.BLIND.THE_INSECURITY.ATLAS_ROW },
+	boss_colour = AST.BLIND.THE_INSECURITY.COLOR,
+	dollars = AST.BLIND.THE_INSECURITY.REWARD,
+	mult = AST.BLIND.THE_INSECURITY.BASE_MULT,
+	boss = { min = AST.BLIND.THE_INSECURITY.BOSS_MIN, max = AST.BLIND.THE_INSECURITY.BOSS_MAX },
+	recalc_debuff = function(_, _, _)
+		if G.GAME.current_round.last_obtained_joker_unique_val ~= 0 then
+			local joker = AST.find_joker_by_unique_val(G.GAME.current_round.last_obtained_joker_unique_val)
+			if joker then
+				joker:set_debuff(true)
+				joker:juice_up()
+			end
+		end
+	end,
+	disabled = function(_)
+		if G.GAME.current_round.last_obtained_joker_unique_val ~= 0 then
+			local joker = AST.find_joker_by_unique_val(G.GAME.current_round.last_obtained_joker_unique_val)
+			if joker then
+				joker:set_debuff(false)
+				joker:juice_up()
+			end
+		end
+	end,
+	defeat = function(_)
+		if G.GAME.current_round.last_obtained_joker_unique_val ~= 0 then
+			local joker = AST.find_joker_by_unique_val(G.GAME.current_round.last_obtained_joker_unique_val)
+			if joker then
+				joker:set_debuff(false)
+				joker:juice_up()
+			end
+		end
+	end
+}
+
+-- Hooking into Card.add_to_deck to get the unique id of most recently obtained joker for The Insecurity boss blind
+local add_to_deck_old = Card.add_to_deck
+function Card:add_to_deck(from_debuff)
+	local ret = add_to_deck_old(self, from_debuff)
+
+	if not from_debuff and self.ability.set == 'Joker' then
+		G.GAME.current_round.last_obtained_joker_unique_val = self.unique_val
+	end
+
+	return ret
+end
+
+-- Hooking into Card.remove_from_deck to set last_obtained_joker_unique_val back to 0 when it's removed from the deck
+local remove_from_deck_old = Card.remove_from_deck
+function Card:remove_from_deck(from_debuff)
+	local ret = remove_from_deck_old(self, from_debuff)
+
+	if not from_debuff and G.GAME.current_round.last_obtained_joker_unique_val == self.unique_val and self.ability.set == 'Joker' then
+		G.GAME.current_round.last_obtained_joker_unique_val = 0
+	end
+
+	return ret
+end
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- The Gambit
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Registering
+SMODS.Blind {
+	key = AST.BLIND.THE_GAMBIT.NAME,
+	atlas = AST.BLIND.ATLAS,
+	pos = { x = 0, y = AST.BLIND.THE_GAMBIT.ATLAS_ROW },
+	boss_colour = AST.BLIND.THE_GAMBIT.COLOR,
+	dollars = AST.BLIND.THE_GAMBIT.REWARD,
+	mult = AST.BLIND.THE_GAMBIT.BASE_MULT,
+	boss = { min = AST.BLIND.THE_GAMBIT.BOSS_MIN, max = AST.BLIND.THE_GAMBIT.BOSS_MAX },
+
+	-- Extra function, added via a lovely patch, which runs for each scored card
+	card_scored = function(self, card)
+		if card:is_face() then
+			G.E_MANAGER:add_event(Event({trigger = 'after', blocking = false, delay = 0.1, func = function()
+				card:juice_up(0.3, 0.5)
+				ease_dollars(-G.GAME.dollars, true)
+				return true
+				end
+			}))
+			return false
+        end
+		return true
+	end
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- The Pit
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Registering
+SMODS.Blind {
+	key = AST.BLIND.THE_PIT.NAME,
+	atlas = AST.BLIND.ATLAS,
+	pos = { x = 0, y = AST.BLIND.THE_PIT.ATLAS_ROW },
+	boss_colour = AST.BLIND.THE_PIT.COLOR,
+	dollars = AST.BLIND.THE_PIT.REWARD,
+	mult = AST.BLIND.THE_PIT.BASE_MULT,
+	boss = { min = AST.BLIND.THE_PIT.BOSS_MIN, max = AST.BLIND.THE_PIT.BOSS_MAX },
+
+	-- Extra function, added via a lovely patch, which runs for each scored card
+	card_scored = function(_, card)
+		if card:get_id() == 2 or card:get_id() == 3 or card:get_id() == 4 or card:get_id() == 5 then
+			G.E_MANAGER:add_event(Event({trigger = 'after', blocking = false, delay = 0.1, func = function()
+				card:juice_up(0.3, 0.5)
+				ease_dollars(-G.GAME.dollars, true)
+				return true
+				end
+			}))
+			return false
+        end
+		return true
+	end
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- The Construct
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Registering
+SMODS.Blind {
+	key = AST.BLIND.THE_CONSTRUCT.NAME,
+	atlas = AST.BLIND.ATLAS,
+	pos = { x = 0, y = AST.BLIND.THE_CONSTRUCT.ATLAS_ROW },
+	boss_colour = AST.BLIND.THE_CONSTRUCT.COLOR,
+	dollars = AST.BLIND.THE_CONSTRUCT.REWARD,
+	mult = AST.BLIND.THE_CONSTRUCT.BASE_MULT,
+	boss = { min = AST.BLIND.THE_CONSTRUCT.BOSS_MIN, max = AST.BLIND.THE_CONSTRUCT.BOSS_MAX },
+	recalc_debuff = function(_, card, _)
+		return card.area ~= G.jokers and AST.is_prime(card.base.nominal)
+	end
+}
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Common Hooks
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Hooking to Game.init_game_object to register extra data for the boss blinds
+local igo = Game.init_game_object
+function Game:init_game_object()
+	local ret = igo()
+	ret.current_round.the_clock = {
+		remaining_time = 0,
+		paused = true,
+		timer_ui_text = nil,
+		timer_text = '0:00',
+		hand_is_being_played = false
+	}
+	ret.current_round.last_obtained_joker_unique_val = 0
+	return ret
+end
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+-- blind.lua End
+------------------------------------------------------------------------------------------------------------------------------------------------------
